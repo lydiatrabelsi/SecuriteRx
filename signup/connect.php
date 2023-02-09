4f4204d32b93c228a53d2360b6b47d8520880f6f
Trabelsi Lydia <lydtrabelsi@gmail.com>
2023-02-09 14:02:20 +0100

    adding signup

---
+++ signup/connect.php
@@ -0,0 +1,15 @@
+<?php
+$HOSTNAME='localhost';
+$USERNAME='root';
+$PASSWORD='';
+$DATABASE='singupforms';
+
+$con=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
+
+if($con){
+    echo "Connection successful";
+}else{
+    die(mysqli_error($con));
+}
+
+?>
